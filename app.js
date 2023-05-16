d19ca63b37e23b6b81b56a8c0cb5f495e52d0996
Ϳαγ ᾥίῘῘίαm Sαρατ <jaywilliamsapar@gmail.com>
2023-05-16 16:19:46 +0800

    Add cars model
    boy lamog
    
    Co-Authored-By: Jenny Guangco <132253817+devjaywilliamsapar@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,24 @@
+// Array of car models
+var carModels = [
+    'Toyota Camry',
+    'Honda Civic',
+    'Ford Mustang',
+    'Chevrolet Corvette',
+    'BMW 3 Series',
+    'Mercedes-Benz C-Class',
+    'Audi A4',
+    'Volkswagen Golf',
+    'Tesla Model S',
+    'Nissan Altima'
+  ];
+  
+  // Accessing car models
+  console.log(carModels[0]);  // Output: Toyota Camry
+  console.log(carModels[1]);  // Output: Honda Civic
+  console.log(carModels[2]);  // Output: Ford Mustang
+  // ...
+  
+  // Iterating over car models
+  carModels.forEach(function(model) {
+    console.log(model);
+  });
